c1fd321b7710f13d142580a3a5412b37d92c5c25
Lane Enget <lane.enget@go.minneapolis.edu>
2020-04-23 16:59:49 -0500

    student sign in app with API

---
+++ student_sign_in_client/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import StudentAPIService from '@/services/StudentServices'
+
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.prototype.$student_api = StudentAPIService
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
